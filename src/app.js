b1b3947ce5c81f8c2095ede4bea8ab5cb1d05508
vijay kasar <vijaykasar27@gmail.com>
2026-04-14 16:01:30 +0530

    feat: create employee endpoint

--- src/app.js
+++ src/app.js
@@ -3,4 +3,8 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.post("/employees", (req, res) => {
+  res.status(201).json(req.body);
+});
+
 module.exports = app;
